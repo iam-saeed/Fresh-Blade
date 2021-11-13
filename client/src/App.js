f2955a1d081841115e21cd8e4c637735a7aa57e6
Saeed Khan <saeed@skdevelopment.org>
2021-11-13 14:05:08 -0700

    home page done

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import Navbar from './components/Nav/Navbar.jsx';
+import Home from './components/Home/Home.jsx';
+import './App.css'
 
 function App() {
   return (
@@ -6,6 +8,7 @@ function App() {
       <header className="App-header">
         <Navbar />
       </header>
+      <Home />
     </div>
   );
 }
---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './styles.css'
+import barberIcon from '../../assets/rubber-good-job-1.png'
+
+const Home = () => {
+    return (
+        <section id="landing_page_section">
+            <div className="container">
+            <h1>Look Good. Feel Good.</h1>
+            <p>Discover barbers near you.</p>
+            <button>Find A Professional</button>
+            </div>
+            
+        </section>
+    )
+}
+
+export default Home
